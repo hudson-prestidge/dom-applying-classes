a32f6ed3f988c4650ad9a671fa948f5c8ccfd5d2
hudson-prestidge <charcon@ihug.co.nz>
2016-06-16 19:52:19 +1200

    implement makeVisible()

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -32,3 +33,9 @@ function three () {
   three.classList.add('red')
 }
 // CREATE FUNCTION makeVisible HERE
+
+function makeVisible () {
+  var four = document.getElementsByClassName('invisible')[0]
+  four.classList.remove('invisible')
+  four.classList.add('visible')
+}
